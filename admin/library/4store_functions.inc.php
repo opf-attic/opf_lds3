dc883ecbcecd1a4cadea9e23b0bc84ded5ddd5bb
Dave Tarrant <davetaz@gmail.com>
2012-04-04 11:14:03 +0100

    Added call to flush puelia cache directory

--- admin/library/4store_functions.inc.php
+++ admin/library/4store_functions.inc.php
@@ -6,7 +6,10 @@ function addDocumentToIndex($final_file_path,$subject) {
 	include('config.php');
 
 	$cmd = 'curl -T ' . $final_file_path . " " . $index_edit_url . $subject;
+
 	exec($cmd);
+
+	clearPueliaCache();
 
 }
 
@@ -18,7 +21,24 @@ function removeDocumentFromIndex($previous) {
 
         exec($cmd);
 
+	clearPueliaCache();
+
 	
+}
+
+function clearPueliaCache() {
+
+	include('config.php');
+		
+	$d = dir($puelia_cache_dir);
+	
+	while ($entry = $d->read()) {
+		if ($entry!= "." && $entry != "..") {
+			unlink($puelia_cache_dir . "/" . $entry);
+		}
+	}
+	$d->close();
+
 }
 	
 ?>
