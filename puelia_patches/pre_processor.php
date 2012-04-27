1e63225bad2bdac1497ffc2f06f31f33b56955b6
Dave Tarrant <davetaz@gmail.com>
2012-04-27 22:38:40 +0100

    Added Content negotiation and ability to download raw RDF files from server directories to pre_processor

--- puelia_patches/pre_processor.php
+++ puelia_patches/pre_processor.php
@@ -1,4 +1,5 @@
 <?php
+
 if ($_SERVER["REQUEST_URI"] == "/admin/" || $_SERVER["REQUEST_URI"] == "/admin") {
 	header("Location: http://" . $_SERVER['HTTP_HOST'] . "/admin/index.php");
 	exit();
@@ -14,14 +15,37 @@ include_once('functions.inc.php');
 
 $length = strlen($http_doc_prefix . "CA/7D/A10B/FBA0-4A01-B62B-B4737B8AA180");
 $requested_uri = "http://" . $base_domain . $_SERVER["REQUEST_URI"];
+
+$local = getLocalFromURI($requested_uri);
+if (file_exists($local) && !is_dir($local)) {
+	header('Content-type: application/rdf+xml');
+	$content = file_get_contents($local);
+	echo $content;
+	exit();
+}
+
+$accept_header = $_SERVER["HTTP_ACCEPT"];
+$accept_parts = explode(",",$accept_header);
+$accept = $accept_parts[0];
+
 if (substr($requested_uri,strlen($requested_uri)-1,strlen($requested_uri)) == "/") {
 	$requested_uri = substr($requested_uri,0,strlen($requested_uri)-1);
+}
+
+if (trim($accept) == "application/rdf+xml" && is_file($local.".rdf")) {
+	header("HTTP/1.1 303 See Other");
+	header("Location: $requested_uri.rdf");
+	exit();
 }
 
 if (substr($requested_uri, 0, strlen($http_doc_prefix)) == $http_doc_prefix && strlen($requested_uri) == $length) {
 	$uri = getAbsoluteLatestVersionURI($requested_uri . "/latest");
 	$uri = substr($uri,0,-4);
 	header("HTTP/1.1 303 See Other");
+	if (trim($accept) == "application/rdf+xml") {
+		header("Location: $uri.rdf");
+		exit();
+	}
 	header("Location: $uri");
         exit();
 }
